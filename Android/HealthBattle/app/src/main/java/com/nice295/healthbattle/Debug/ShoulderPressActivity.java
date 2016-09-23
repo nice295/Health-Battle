a3d89ddbaafe604de5b368d020906e21067bd314
Argo <sglee77@naver.com>
2016-09-24 03:35:42 +0900

    1.5f

--- Android/HealthBattle/app/src/main/java/com/nice295/healthbattle/Debug/ShoulderPressActivity.java
+++ Android/HealthBattle/app/src/main/java/com/nice295/healthbattle/Debug/ShoulderPressActivity.java
@@ -65,7 +65,7 @@ public class ShoulderPressActivity extends BaseActivity
      * measured by the Gravity sensor, changes with a variation (delta) > GRAVITY_THRESHOLD,
      * we consider that a successful count.
      */
-    private static final float GRAVITY_THRESHOLD = 1.0f; //7.0f;
+    private static final float GRAVITY_THRESHOLD = 1.5f; //7.0f;
 
     private DatabaseReference mDatabase;
     private DatabaseReference myRef;
